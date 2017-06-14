902fd0b0172e03c3860d56c86b123a5026870ab0
Nian Liu <nliu@bol.com>
2017-06-14 22:36:38 +0200

    fibonacci sequence app

---
+++ src/main/java/chapter1/fibonacci/Fibonacci.java
@@ -0,0 +1,17 @@
+package chapter1.fibonacci;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int low = 1;
+        int high = 1;
+
+        System.out.println(low);
+
+        while (high < 50) {
+            System.out.println(high);
+            high = low + high;
+            low = high - low;
+        }
+    }
+}
